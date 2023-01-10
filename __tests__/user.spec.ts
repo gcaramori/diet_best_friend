ccc37de144bbfb1eea4f00f1c63c12943117ec57
Guilherme Caramori <gcaramori16@gmail.com>
2023-01-09 23:17:35 -0300

    First commit 🎉

---
+++ __tests__/user.spec.ts
@@ -0,0 +1,7 @@
+import { double } from '../src/index';
+
+describe('testing index file', () => {
+    test('double function', () => {
+        expect(double(5)).toBe(10);
+    });
+});
---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/**/__tests__/**/*.spec.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  coverageDirectory: './coverage',
+  coveragePathIgnorePatterns: ['node_modules', 'src/database', 'src/test', 'src/types'],
+  reporters: ['default', 'jest-junit']
+};

199eb510b4891f2ca5cf17f5ef848354bd954aa1
Guilherme Caramori <gcaramori16@gmail.com>
2023-01-11 21:07:33 -0300

    Connected with localhost sql

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,20 @@
 import express, { Express, Response, Request } from 'express';
 import dotenv from 'dotenv';
 import 'reflect-metadata';
+import ormConfig from './src/config/ormconfig';
 
 dotenv.config();
 
 const app: Express = express();
 const PORT = process.env.PORT || 3001;
+
+ormConfig.initialize()
+.then(() => {
+    console.log("Data Source has been initialized!")
+})
+.catch((err) => {
+    console.error("Error during Data Source initialization", err)
+});
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).send('Express + TypeScript Server');
--- server/src/config/ormconfig.ts
+++ server/src/config/ormconfig.ts
@@ -1,11 +1,16 @@
 import { DataSource } from "typeorm";
 import dotenv from "dotenv";
+import { parse } from "path";
 
 dotenv.config();
 
 const ormConfig = new DataSource({
     type: 'mysql',
-    url: process.env.DATABASE_URI,
+    host: process.env.DB_HOST,
+    port: 3306,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     logging: false,
     synchronize: true,
     entities: ["./modules/**/*/infrastructure/typeorm/entity/*.ts"],

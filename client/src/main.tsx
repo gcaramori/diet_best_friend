1f3ccfb8bb363718c778e53c9650b846979c73f0
Guilherme <guilherme@sunsetsolutions.com.br>
2023-02-02 16:35:11 -0300

    Developing login page 🤩

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ChakraProvider } from '@chakra-ui/react'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import theme from './lib/theme'
 import '@fontsource/raleway'
@@ -11,9 +12,11 @@ import AuthProvider from './contexts/AuthContext'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <GoogleOAuthProvider clientId="990577240690-0rk8l2262b07hrpuutfg5tuj55ag2clc.apps.googleusercontent.com">
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </GoogleOAuthProvider>
     </ChakraProvider>
   </React.StrictMode>
 )
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
-import { Flex, Image, Heading } from '@chakra-ui/react'
+import { Flex, Image, Heading, Divider, Button } from '@chakra-ui/react'
+import { useGoogleLogin } from '@react-oauth/google'
 import CustomForm from '../components/CustomForm'
+import { FcGoogle } from 'react-icons/fc'
 
 const Login: React.FC = () => {
+    const login = useGoogleLogin({
+        onSuccess: tokenResponse => console.log(tokenResponse),
+    });
+
     return (
         <Flex justifyContent='center' alignItems='center' w='100%' h='100%' bg='mainBlue'>
             <Flex direction="column" alignItems='center' gap={10} w={{ base: '95%', md: '60%' }} maxW={{ md: '500px' }} h={{ base: '80%', md: '85%' }} bg='mainWhite' rounded={50} boxShadow='lg' py={10} px={20}>
-                <Image src="logo.png" alt="logo_image" w='250px' h='250px' objectFit='contain'/>
+                <Image src="logo.png" alt="logo_image" w='220px' h='220px' objectFit='contain'/>
                 
                 <Flex direction='column' justifyContent='center' alignItems='center' w='100%'>
                     <Heading as='h2' fontSize={32} mb={10}>Bem vindo de volta!</Heading>
                     
                     <CustomForm />
+
+                    <Divider my={6} />
+
+                    <Button w={12} h={12} rounded='lg' shadow='lg' fontSize={16} onClick={() => login()}>
+                        <FcGoogle />
+                    </Button>
                 </Flex>
             </Flex>
         </Flex>
